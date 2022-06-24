027e14c5ae3afa59df54736be65f5734a6a7e800
Yuri Silva <yusilva12345@gmail.com>
2022-06-24 10:27:03 -0300

    feat: 🎸 add provider react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
-
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
 import { ThemeProvider } from 'styled-components'
-import Routes from './routes'
 
+import Routes from './routes'
 import Global from './styles/global'
 import theme from './styles'
+import { queryClient } from './lib/query'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Routes />
-        <Global />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Routes />
+          <Global />
+        </ThemeProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
